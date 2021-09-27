1734e06c23d825d5b2ce160c03eb85be26f644ae
Dino <dc@estudiocrescimbeni.com.ar>
2021-09-27 13:09:44 -0300

    finished reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,7 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let s = '';
+  for (let i = 0; i < str.length; i++) {
+    s += str[str.length - 1 - i].toString();
+  }
+  return s;
+}
 
 module.exports = reverse;
-// test commit
