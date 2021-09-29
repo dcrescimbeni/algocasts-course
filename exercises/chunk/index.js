1c155355dc033c71d212ba7bb3775a95eef9aa6f
dcrescimbeni <dc@estudiocrescimbeni.com.ar>
2021-09-29 07:33:56 -0300

    finished chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,31 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let arrParent = [];
+  let arrChild = [];
+  let count = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    arrChild.push(array[i]);
+    count++;
+    // console.log(arrChild);
+    if (count === size) {
+      arrParent.push(arrChild);
+      arrChild = [];
+      count = 0;
+    }
+  }
+  if (arrChild.length !== 0) {
+    arrParent.push(arrChild);
+  }
+  console.log(arrParent);
+  return arrParent;
+}
+
+chunk([1, 2, 3, 4, 5, 6], 2);
+chunk([1, 2, 3, 4, 5, 6], 2);
+chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3);
+chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 20);
 
 module.exports = chunk;
