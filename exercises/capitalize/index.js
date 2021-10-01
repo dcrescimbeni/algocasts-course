d924c6a81f479818e6456b8d9e9863a21b5f1074
dcrescimbeni <dc@estudiocrescimbeni.com.ar>
2021-10-01 07:37:55 -0300

    finished capitalized

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,17 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let arr = str.split(' ');
+  let capitalizedArr = arr.map((elem) => {
+    let first = elem.substring(0, 1);
+    first = first.toUpperCase();
+    let rest = elem.substring(1, elem.length);
+    return first + rest;
+  });
+  return capitalizedArr.join(' ');
+}
+
+console.log(capitalize('hello, world'));
 
 module.exports = capitalize;
