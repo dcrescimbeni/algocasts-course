a11680e1dd794c9b6f512e6d4217a13a8182c23f
dcrescimbeni <dc@estudiocrescimbeni.com.ar>
2021-10-03 20:50:27 -0300

    finished pyramid exercise

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,33 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // number of characters === n * 2 - 1
+  // loop through pyramid levels
+  for (let i = 1; i <= n; i++) {
+    // square size
+    let size = n * 2 - 1;
+
+    // loop through steps
+    // i is the level
+    // i * 2 - 1 is the number of "#" of the step
+    // the difference is white space
+    // starting point
+    let starting = (size - (i * 2 - 1)) / 2;
+    let finishing = i * 2 - 1 + starting;
+
+    let str = '';
+    for (let j = 1; j <= size; j++) {
+      if (j > starting && j <= finishing) {
+        str += '#';
+      } else {
+        str += ' ';
+      }
+    }
+    console.log(str);
+  }
+}
+
+pyramid(10);
 
 module.exports = pyramid;
