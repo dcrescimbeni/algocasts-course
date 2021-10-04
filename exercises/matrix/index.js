e2bbb8e903fb9c507f89e5ffc6c216102b8242ff
Dino <dc@estudiocrescimbeni.com.ar>
2021-10-04 08:20:08 -0300

    finished the damned matrix exercise im a god

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,75 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  // Create empty matrix
+  let result = [];
+  for (x = 0; x < n; x++) {
+    let arrX = [];
+    for (y = 0; y < n; y++) {
+      arrX[y] = 'X';
+    }
+    result.push(arrX);
+  }
+
+  // Trackers initialization
+  let num = 1;
+  // row
+  let startRowGlobal = 0;
+  let endRowGlobal = n - 1;
+  // column
+  let startColumnGlobal = 0;
+  let endColumnGlobal = n - 1;
+
+  // bigger loop to increment num
+  while (num <= n * n) {
+    // current pointers initialization
+    let rowCurrent;
+    let columnCurrent;
+
+    // row-normal fill
+    columnCurrent = startColumnGlobal;
+    while (columnCurrent <= endColumnGlobal) {
+      result[startRowGlobal][columnCurrent] = num;
+      columnCurrent++;
+      num++;
+    }
+    startRowGlobal++;
+
+    // column-normal fill
+    rowCurrent = startRowGlobal;
+    while (rowCurrent <= endRowGlobal) {
+      result[rowCurrent][endRowGlobal] = num;
+      rowCurrent++;
+      num++;
+    }
+    endColumnGlobal--;
+
+    // row-backwards fill
+    columnCurrent = endColumnGlobal;
+    while (columnCurrent >= startColumnGlobal) {
+      result[endRowGlobal][columnCurrent] = num;
+      columnCurrent--;
+      num++;
+    }
+    endRowGlobal--;
+
+    // column-backwards fill
+    rowCurrent = endRowGlobal;
+    while (rowCurrent >= startRowGlobal) {
+      result[rowCurrent][startColumnGlobal] = num;
+      rowCurrent--;
+      num++;
+    }
+    startColumnGlobal++;
+
+    console.log('loop');
+  }
+
+  console.log(num);
+  console.log(result);
+}
+
+matrix(6);
 
 module.exports = matrix;
