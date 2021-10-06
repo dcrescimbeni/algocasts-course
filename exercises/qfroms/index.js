d48e83f18f73022cd9563d1a01d4f87ae27a39fc
dcrescimbeni <dc@estudiocrescimbeni.com.ar>
2021-10-06 08:01:39 -0300

    finished queue from stack

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,57 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stackOne = new Stack();
+    this.stackTwo = new Stack();
+  }
+
+  add(record) {
+    this.stackOne.push(record);
+  }
+
+  remove() {
+    // Pass all the records from stackOne to stackTwo
+    while (this.stackOne.peek()) {
+      this.stackTwo.push(this.stackOne.pop());
+    }
+
+    // Remove the top record of stackTwo
+    let removed = this.stackTwo.pop();
+
+    // Return all the records from stackTwo to stackOne
+    while (this.stackTwo.peek()) {
+      this.stackOne.push(this.stackTwo.pop());
+    }
+
+    return removed;
+  }
+
+  peek() {
+    // Pass all the records from stackOne to stackTwo
+    while (this.stackOne.peek()) {
+      this.stackTwo.push(this.stackOne.pop());
+    }
+
+    // Peek first record of stackTwo
+    let peekValue = this.stackTwo.peek();
+
+    // Return all the recods from stackTwo to stackOne
+    while (this.stackTwo.peek()) {
+      this.stackOne.push(this.stackTwo.pop());
+    }
+
+    return peekValue;
+  }
+}
+
+let q = new Queue();
+
+q.add(1);
+q.add(2);
+q.add(3);
+
+console.log(q.remove());
 
 module.exports = Queue;
