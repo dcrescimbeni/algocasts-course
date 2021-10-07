5267feabdab44ab7d9916574420a418d9d851da7
dcrescimbeni <dc@estudiocrescimbeni.com.ar>
2021-10-06 22:13:30 -0300

    linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,36 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  insertFirst(data) {
+    let oldHead = this.head;
+    this.head = new Node(data, oldHead);
+  }
+
+  size() {
+    let count = 0;
+    if (this.head) {
+      count++;
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+        count++;
+      }
+    }
+
+    return count;
+  }
+}
 
 module.exports = { Node, LinkedList };
