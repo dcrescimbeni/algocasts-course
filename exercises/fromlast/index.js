a24cc8b526c4cae1e6e50f4e7f9044dbdc9a7f89
dcrescimbeni <dc@estudiocrescimbeni.com.ar>
2021-10-14 19:54:56 -0300

    finihed fromlast

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,23 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  let slow = list.head;
+  let fast = list.head;
+
+  // Advance fast
+  let i = 0;
+  while (i < n) {
+    fast = fast.next;
+    i++;
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
+}
 
 module.exports = fromLast;
