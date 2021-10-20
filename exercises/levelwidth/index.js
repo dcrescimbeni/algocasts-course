b65e475a621a7ce5ae3fd76202898a2c1aff0a06
Dino <dc@estudiocrescimbeni.com.ar>
2021-10-20 08:05:46 -0300

    finished levelwidth excercise

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,40 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let stack = [root, 's'];
+  let counter = [0];
+
+  while (stack.length > 1) {
+    let current = stack.shift();
+
+    // process 's'
+    if (current === 's') {
+      counter.push(0);
+      stack.push('s');
+    } else {
+      // increment counter
+      counter[counter.length - 1]++;
+
+      // add children
+      if (current.children) {
+        stack.push(...current.children);
+      }
+    }
+  }
+
+  return counter;
+}
+
+// const Node = require('./node');
+
+// const root = new Node(0);
+// root.add(1);
+// root.add(2);
+// root.add(3);
+// root.children[0].add(4);
+// root.children[2].add(5);
+
+// console.log(levelWidth(root));
 
 module.exports = levelWidth;
