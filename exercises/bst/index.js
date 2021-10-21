86c331ab46f2a012d5baab30252eb8c5a9c96005
Dino <dc@estudiocrescimbeni.com.ar>
2021-10-20 21:28:35 -0300

    Finished node implementation

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,95 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    function insert(data, current) {
+      // Less than (left)
+      if (current.data > data) {
+        if (!current.left) {
+          current.left = new Node(data);
+          return;
+        }
+
+        if (current.left) {
+          current = current.left;
+          insert(data, current);
+        }
+      }
+
+      // Greater than (right)
+      if (current.data < data) {
+        if (!current.right) {
+          current.right = new Node(data);
+          return;
+        }
+
+        if (current.right) {
+          current = current.right;
+          insert(data, current);
+        }
+      }
+    }
+
+    insert(data, this);
+  }
+
+  contains(toFind) {
+    function search(toFind, current) {
+      // Base case, we find the value
+      if (current.data === toFind) {
+        return current;
+      }
+
+      // Left
+      if (current.data > toFind) {
+        // The node has no children
+        // So it doesn't exist
+        if (!current.left) {
+          return null;
+        }
+
+        if (current.left) {
+          current = current.left;
+          return search(toFind, current);
+        }
+      }
+
+      // Right
+      if (current.data < toFind) {
+        // The node has no children
+        // So it doesn't exist
+        if (!current.right) {
+          return null;
+        }
+
+        if (current.right) {
+          current = current.right;
+          return search(toFind, current);
+        }
+      }
+    }
+
+    // console.log(search(toFind, this));
+
+    return search(toFind, this);
+  }
+}
+
+const node = new Node(10);
+node.insert(5);
+node.insert(15);
+node.insert(20);
+node.insert(0);
+node.insert(-5);
+node.insert(3);
+
+node.contains(5);
 
 module.exports = Node;
